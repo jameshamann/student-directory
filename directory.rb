eef5102ac0e2857b603a19562d35966fc5fdd336
jameshamann <jameshamann@me.com>
2016-08-01 17:21:00 +0100

    Adding an Array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,27 @@
-student_count = 11
-#first we print the list of students and the name of the school
+
+#Put the students into an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffreu Baratheon",
+  "Norman Bates"
+]
+
+#and now we print them out
+
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffreu Baratheon"
-puts "Norman Bates"
+#iteration, each element of the array is assigned to 'student' and the code
+#between do and end is executed
+students.each do |student|
+  puts student
+end
 #finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students!"
