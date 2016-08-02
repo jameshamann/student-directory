6ace4ab1b7677406690031ea98d3e15f5b1c31e0
jameshamann <jameshamann@me.com>
2016-08-02 17:29:57 +0100

    Inclusion of additional student details

--- directory.rb
+++ directory.rb
@@ -1,17 +1,33 @@
 def input_students
-  puts "Please enter the names of the students".center(100)
-  puts "To finish, just hit return twice".center(100)
+  puts "Please enter the name, birth place, height and age of the students".center(100)
+  puts "To finish, just simply hit return 4 times".center(100)
   #create an empty array
   students = []
   #ask for the first name
+  puts
+  puts 'Please enter your first and last name'.center(100)
   name = gets.chomp
+  puts 'Awesome! Now what is your approximate height (in meters, using numbers)'.center(100)
+  height = gets.chomp
+  puts 'Nice! Please enter your age (again, using numbers please)'.center(100)
+  age = gets.chomp
+  puts 'Great! Where were you born?'.center(100)
+  location = gets.chomp
   #while the name is not empty, do this code:
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students".center(100)
+    students << {name: name, cohort: :november, location: location, height: height, age: age}
+    puts "Now we have #{students.count} people enrolled".center(100)
     #get another name from the user
+    puts "And the same again".center(100)
+    puts 'Please enter your first and last name'.center(100)
     name = gets.chomp
+    puts 'Awesome! Now what is your approximate height (in meters, using numbers)'.center(100)
+    height = gets.chomp
+    puts 'Nice! Please enter your age (again, using numbers please)'.center(100)
+    age = gets.chomp
+    puts 'Great! Where were you born?'.center(100)
+    location = gets.chomp
   end
   students
 end
@@ -22,12 +38,18 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index {|student, index| puts "#{index} : #{student[:name]} (#{student[:cohort]} cohort)".center(100)}
+  students.each_with_index {|student, index| puts "#{index} : #{student[:name]} , #{student[:height]} meters , #{student[:age]} years of age, born in #{student[:location]} , (#{student[:cohort]} cohort)".center(100)}
 end
 
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students!".center(100)
+  if students.count < 2
+  puts
+  puts "Overall, we have #{students.count} great student!".center(100)
+  else
+    puts
+    puts "Overall, we have #{students.count} great students!".center(100)
+  end
 end
 
 students = input_students
